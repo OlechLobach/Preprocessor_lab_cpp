326986cb49c1438cf3fad3963a411fc5af0535e2
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2023-11-24 00:10:39 +0200

    initiall commit

--- func/func.h
+++ func/func.h
@@ -1,1 +1,16 @@
-#pragma once
+#ifndef FUNC_H
+#define FUNC_H
+#include <iostream>
+#include <cmath>
+
+
+
+#define MIN(x, y) ((x) < (y) ? (x) : (y))
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
+#define SQUARE(x) ((x) * (x))
+#define POWER(x, n) (pow(x, n))
+#define IS_EVEN(x) ((x) % 2 == 0)
+#define IS_ODD(x) ((x) % 2 != 0)
+
+#endif
+
--- main/main.cpp
+++ main/main.cpp
@@ -1,7 +1,51 @@
-#include<iostream>
 
-int main()
-{
+#include "../func/func.h"
 
-	return 0;
+using namespace std;
+
+int main() {
+    int choice, a, b;
+    cout << "Choose function:\n";
+    cout << "1. Minimum\n";
+    cout << "2. Maximum\n";
+    cout << "3. Square\n";
+    cout << "4. Power\n";
+    cout << "5. Check even or odd\n";
+    cin >> choice;
+
+    switch (choice) {
+    case 1:
+        cout << "Enter two numbers: ";
+        cin >> a >> b;
+        cout << "Minimum: " << MIN(a, b) << endl;
+        break;
+    case 2:
+        cout << "Enter two numbers: ";
+        cin >> a >> b;
+        cout << "Maximum: " << MAX(a, b) << endl;
+        break;
+    case 3:
+        cout << "Enter a number: ";
+        cin >> a;
+        cout << "Square: " << SQUARE(a) << endl;
+        break;
+    case 4:
+        cout << "Enter a number: ";
+        cin >> a;
+        int power;
+        cout << "Enter a power: ";
+        cin >> power;
+        cout << "Power: " << POWER(a, power) << endl;
+        break;
+    case 5:
+        cout << "Enter a number: ";
+        cin >> a;
+        cout << a << " is " << (IS_EVEN(a) ? "even" : "odd") << endl;
+        break;
+    default:
+        cout << "Invalid choice.\n";
+        break;
+    }
+
+    return 0;
 }
